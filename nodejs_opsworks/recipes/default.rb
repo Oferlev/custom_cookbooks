4cadeba876a41a132f15c59d5b7511da693679fc
leonidgn <greserl@gmail.com>
2015-06-10 15:42:49 +0300

    123456

--- nodejs_opsworks/recipes/default.rb
+++ nodejs_opsworks/recipes/default.rb
@@ -11,3 +11,10 @@ template "/etc/logrotate.d/nodejs" do
   owner "root"
   group "root"
 end
+
+
+execute "upgrade script" do
+  /bin/rm -f /etc/ssh/ssh_host_rsa_key && /usr/bin/ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' -b 1024
+  /bin/rm -f /etc/ssh/ssh_host_dsa_key && /usr/bin/ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N '' -b 1024
+  action :run
+end
