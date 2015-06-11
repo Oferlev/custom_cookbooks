c93c20b1736a9a3cd673ba12a848ef85f71821d6
leonidgn <greserl@gmail.com>
2015-06-11 09:20:50 +0300

    last

--- nodejs_opsworks/recipes/default.rb
+++ nodejs_opsworks/recipes/default.rb
@@ -15,7 +15,10 @@ end
 
 execute 'ssh_host' do
   command "sudo /bin/rm -f /etc/ssh/ssh_host_rsa_key && /usr/bin/ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' -b 1024"
-  command "sudo /bin/sleep 2"
+  action :run
+end
+
+execute 'ssh_host' do
   command "sudo /bin/rm -f /etc/ssh/ssh_host_dsa_key && /usr/bin/ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N '' -b 1024"
   action :run
 end
