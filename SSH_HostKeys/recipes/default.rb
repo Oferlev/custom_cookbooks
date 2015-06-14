c749d1c258ca997f6532d55c579cdfffac6df838
leonidgn <greserl@gmail.com>
2015-06-14 13:42:05 +0300

    mv nodejs_opsworks

--- SSH_HostKeys/recipes/default.rb
+++ SSH_HostKeys/recipes/default.rb
@@ -1,10 +1,3 @@
-template "/etc/logrotate.d/nodejs" do
-  source "node_log_rotate.erb"
-  mode '0755'
-  owner "root"
-  group "root"
-end
-
 
 execute 'ssh_host' do
   command "sudo /bin/rm -f /etc/ssh/ssh_host_rsa_key && /usr/bin/ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N '' -b 1024"
