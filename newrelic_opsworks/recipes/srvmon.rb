9758d158ef6c3e6ef653d6e68ba196a5aae7246f
leonidgn <greserl@gmail.com>
2015-06-18 17:48:02 +0300

    newrelic_nrsysmond

--- newrelic_opsworks/recipes/srvmon.rb
+++ newrelic_opsworks/recipes/srvmon.rb
@@ -1,6 +1,6 @@
 
 execute 'repo_install' do
-  command "sudo /bin/rpm -Uvh " <%= node[:newrelic][:repo_inst_url] %> 
+  command "sudo /bin/rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm "  
   action :run
 end
 
