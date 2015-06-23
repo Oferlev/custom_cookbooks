b0180dc711c0f2938a884a5672a0d6740ae20fc6
leonidgn <greserl@gmail.com>
2015-06-23 13:48:43 +0300

    deploy

--- newrelic_opsworks/attributes/newrelic.rb
+++ newrelic_opsworks/attributes/newrelic.rb
@@ -2,6 +2,6 @@ default[:newrelic][:repo_inst_url] = 'http://yum.newrelic.com/pub/newrelic/el5/x
 default[:newrelic][:license_key] = '0000ffff0000ffff0000ffff0000ffff0000ffff'
 default[:newrelic][:api_key] = '0000ffff0000ffff0000ffff0000ffff0000ffff'
 default[:newrelic][:app_name] = 'Proxy'
-default[:newrelic][:gits_url] = 'https://github.com/ConduitMiddlewareServices/cep_traffic_proxy.git'
+default[:newrelic][:gits_url] = 'http://github.com/'
 
 
--- newrelic_opsworks/recipes/rep-deploy.rb
+++ newrelic_opsworks/recipes/rep-deploy.rb
@@ -2,15 +2,11 @@ bash 'deploy_report' do
 	user 'root'
  	cwd '/tmp'
 	code <<-EOH
+
     app_revision=`sudo /usr/bin/git ls-remote #{node[:newrelic][:gits_url]} refs/heads/master|awk '{print $1 }'`
-    echo "app_revision=`sudo /usr/bin/git ls-remote #{node[:newrelic][:gits_url]} refs/heads/master|awk '{print $1 }'`" > /tmp/test222
      
 	sudo /usr/bin/curl  -H  "x-api-key:#{node[:newrelic][:api_key]}"  -d "deployment[app_name]=#{node[:newrelic][:app_name]}" -d "deployment[revision]=${app_revision:0:10}" -d "deployment[user]=OpsWorks"  https://api.newrelic.com/deployments.xml
-	echo "sudo /usr/bin/curl  -H  "x-api-key:#{node[:newrelic][:api_key]}"  -d "deployment[app_name]=#{node[:newrelic][:app_name]}" -d "deployment[revision]=${app_revision:0:10}" -d "deployment[user]=OpsWorks"  https://api.newrelic.com/deployments.xml" >> /tmp/test222
+	
 	EOH
 end
 
-execute 'repo_install' do
-  command "echo #{node[:newrelic][:repo_inst_url]} >> /tmp/test222 " 
-  action :run
-end
