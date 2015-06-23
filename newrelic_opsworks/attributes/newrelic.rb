338c74470b1b0be6ce98eef5122c65037009cddd
leonidgn <greserl@gmail.com>
2015-06-23 11:05:55 +0300

    deploy

--- newrelic_opsworks/attributes/newrelic.rb
+++ newrelic_opsworks/attributes/newrelic.rb
@@ -1,3 +1,7 @@
 default[:newrelic][:repo_inst_url] = 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm'
 default[:newrelic][:license_key] = '0000ffff0000ffff0000ffff0000ffff0000ffff'
+default[:newrelic][:api_key] = '0000ffff0000ffff0000ffff0000ffff0000ffff'
+default[:newrelic][:app_name] = 'Proxy'
+default[:newrelic][:gits_url] = 'https://github.com/ConduitMiddlewareServices/cep_traffic_proxy.git'
+
 
