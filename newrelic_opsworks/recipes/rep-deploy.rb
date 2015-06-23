939df2fd62540fe3bd110aa89a1081ba3e763bf3
leonidgn <greserl@gmail.com>
2015-06-23 13:56:22 +0300

    deploy

--- newrelic_opsworks/recipes/rep-deploy.rb
+++ newrelic_opsworks/recipes/rep-deploy.rb
@@ -1,6 +1,17 @@
 bash 'deploy_report' do
 	user 'root'
  	cwd '/tmp'
+	case node[:platform]
+  when "centos","redhat","fedora","suse"
+    code <<-EOH
+    echo "centos" > /tmp/test222 
+    EOH
+  when "debian","ubuntu"
+    code <<-EOH
+    echo "debian" > /tmp/test222 
+    EOH
+  end
+	
 	code <<-EOH
 
     app_revision=`sudo /usr/bin/git ls-remote #{node[:newrelic][:gits_url]} refs/heads/master|awk '{print $1 }'`
