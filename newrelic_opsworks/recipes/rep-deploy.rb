488607b8c929bfc1f5438994357b9c01ac726066
leonidgn <greserl@gmail.com>
2015-06-23 19:01:40 +0300

    deploy

--- newrelic_opsworks/recipes/rep-deploy.rb
+++ newrelic_opsworks/recipes/rep-deploy.rb
@@ -6,7 +6,11 @@ bash 'deploy_report' do
     app_revision=`sudo /usr/bin/git ls-remote #{node[:newrelic][:gits_url]} refs/heads/master|awk '{print $1 }'`
      
 	sudo /usr/bin/curl  -H  "x-api-key:#{node[:newrelic][:api_key]}"  -d "deployment[app_name]=#{node[:newrelic][:app_name]}" -d "deployment[revision]=${app_revision:0:10}" -d "deployment[user]=OpsWorks"  https://api.newrelic.com/deployments.xml
+
+	echo " ===== #{node['platform']} =====" >  /tmp/test222 
 	
 	EOH
 end
+
+
 
