ee8e6bcff7b4350dfa07cc37ac4106ed643b3ee5
leonidgn <greserl@gmail.com>
2015-06-29 11:11:35 +0300

    deploy

--- newrelic_opsworks/attributes/newrelic.rb
+++ newrelic_opsworks/attributes/newrelic.rb
@@ -1,7 +1,8 @@
-default[:newrelic][:repo_inst_url] = 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm'
+default[:newrelic][:yum_repo] = 'http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm'
 default[:newrelic][:license_key] = '0000ffff0000ffff0000ffff0000ffff0000ffff'
 default[:newrelic][:api_key] = '0000ffff0000ffff0000ffff0000ffff0000ffff'
 default[:newrelic][:app_name] = 'Proxy'
 default[:newrelic][:gits_url] = 'http://github.com/'
 default[:newrelic][:platform] = 'ubuntu'
+default[:newrelic][:apt_repo] = 'https://download.newrelic.com/548C16BF.gpg'
 
--- newrelic_opsworks/recipes/srvmon.rb
+++ newrelic_opsworks/recipes/srvmon.rb
@@ -3,9 +3,14 @@
 
 case node["platform"]
   when "debian", "ubuntu"
+
+    execute 'ubnt_repo_install' do 
+      command "sudo /bin/echo 'deb http://apt.newrelic.com/debian/ newrelic non-free' | sudo /usr/bin/tee /etc/apt/sources.list.d/newrelic.list"
+      action :run
+    end 
  
     execute 'ubnt_repo_install' do 
-      command "sudo /usr/bin/wget -O- https://download.newrelic.com/548C16BF.gpg | sudo /usr/bin/apt-key add - "
+      command "sudo /usr/bin/wget -O- #{node[:newrelic][:apt_repo]}" | sudo /usr/bin/apt-key add - "
       action :run
     end   
 
@@ -24,10 +29,15 @@ case node["platform"]
       action :run
     end
 
+    execute 'newrelic-start' do
+      command "sudo /etc/init.d/newrelic-sysmond start "
+      action :run
+    end  
+
   when "redhat", "centos", "fedora", "amazon"
 
     execute 'repo_install' do
-      command "sudo /bin/rpm -Uvh #{node[:newrelic][:repo_inst_url]} "  
+      command "sudo /bin/rpm -Uvh #{node[:newrelic][:yum_repo]} "  
       not_if { ::File.exists?("/etc/yum.repos.d/newrelic.repo")}
       action :run
     end
